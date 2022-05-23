e145753a312620368b2cfcb1ddb1f50137042870
theliver <kuba.bialy.92@icloud.com>
2022-05-23 19:22:42 +0200

    add initial repl

---
+++ repl/repl.go
@@ -0,0 +1,30 @@
+package repl
+
+import (
+    "bufio"
+    "fmt"
+    "io"
+    "github.com/kubabialy/donkey/lexer"
+    "github.com/kubabialy/donkey/token"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+    scanner := bufio.NewScanner(in)
+
+    for {
+        fmt.Fprintf(out, PROMPT)
+        scanned := scanner.Scan()
+        if !scanned {
+            return
+        }
+
+        line := scanner.Text()
+        lxr := lexer.New(line)
+
+        for tok := lxr.NextToken(); tok.Type != token.EOF; tok = lxr.NextToken() {
+            fmt.Fprintf(out, "%+v\n", tok)
+        }
+    }
+}
