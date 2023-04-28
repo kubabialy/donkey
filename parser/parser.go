91ec4d2d2c3a4cdb793526774f94b1f4a9f71a0c
theliver <kuba.bialy.92@icloud.com>
2023-04-28 22:30:27 +0200

    feat: add grouped expressions

--- parser/parser.go
+++ parser/parser.go
@@ -54,6 +54,7 @@ func New(lxr *lexer.Lexer) *Parser {
 	}
 
 	p.prefixParseFns = make(map[token.TokenType]prefixParseFn)
+    p.registerPrefix(token.LPAREN, p.parseGroupedExpression)
 	p.registerPrefix(token.IDENT, p.parseIdentifier)
 	p.registerPrefix(token.INT, p.parseIntegerLiteral)
 	p.registerPrefix(token.BANG, p.parsePrefixExpression)
@@ -246,6 +247,18 @@ func (p *Parser) parseInfixExpression(left ast.Expression) ast.Expression {
 
 func (p *Parser) parseBoolean() ast.Expression {
 	return &ast.Boolean{Token: p.curToken, Value: p.curTokenIs(token.TRUE)}
+}
+
+func (p *Parser) parseGroupedExpression() ast.Expression {
+    p.NextToken()
+
+    exp := p.parseExpression(LOWEST)
+
+    if !p.expectPeek(token.RPAREN) {
+        return nil
+    }
+
+    return exp
 }
 
 func (p *Parser) curTokenIs(t token.TokenType) bool {
